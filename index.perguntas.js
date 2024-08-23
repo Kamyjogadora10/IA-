c606f46e2001881c70adc9b06b4e43c2ff98f621
Kamyjogadora10 <117648068+Kamyjogadora10@users.noreply.github.com>
2024-08-23 18:00:30 -0300

    Create index.perguntas.js

---
+++ index.perguntas.js
@@ -0,0 +1,61 @@
+export const perguntas = [
+    {
+        enunciado: "Os Vingadores decidiram que teriam que salvar o mundo, detendo a grande ameaça que é o Thanos. O que eles farão?",
+        alternativas: [
+            {
+                texto: "Eles irão atrás dele",
+                afirmacao: ["Decidiram bem rápido o que fazer. ",
+                    "Acharam a melhor opção para fazer isso."
+                ],
+                proxima:1
+            },
+            {
+                texto: "Eles ficaram sentados sem fazer nada",
+                afirmacao: ["Decidiram não se envolver por medo dos resultados.",
+                    "Foram atrás de manter tudo como estava."
+                ],
+                proxima:1
+            }
+        ]
+    },
+    {
+        enunciado: "Com a descoberta de uma possibilidade de mudar o futuro e parar Thanos, eles foram atrás do seu plano. Qual atitude eles devem tomar?",
+        alternativas: [
+            {
+                texto: "Utilizaram uma ferramenta capaz de voltar no espaço tempo.",
+                afirmacao: ["Conseguiu utilizar a IA para buscar informações úteis para salvar o mundo.",
+                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
+                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                ],
+                proxima:2
+            },
+            {
+                texto: "Eles voltaram no passado com a ajuda da IA.",
+                afirmacao: ["Sentiram mais facilidade em consertar a máquina.",
+                    "Acharam muito mais fácil consertar a máquina e assim salvar o mundo.",
+                    "Sentiram um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                ],
+                proxima:2
+            }
+        ]
+    },
+    {
+        enunciado: "Qual filme você deseja assistir?",
+        alternativas: [
+            {
+                filme: "Toy Story",
+                sala: [
+                    "Escolha o seu assento e direcione-se à Sala 5."
+                ],
+                proximo:3
+            },
+            {
+                filme: "Rio"
+                 [
+                    "Escolha o seu assento e direcione-se à Sala 4."
+                ],
+                proximo:3
+            }
+        ]
+    }    
+]
